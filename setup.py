d711788ddc28f0b5acd48b1e20f79dc889c3711c
CatotronExists <cam29706@gmail.com>
2025-01-10 16:34:47 +1100

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 version = '0.1.0'
 description = 'A Python library for interacting with the Apex Legends Live API.'
+long_description = 'A Python library for interacting with the Apex Legends Live API. Includes recieving game events and sending events to interact with the game.'
 
 setup(
     name='LiveApex',
     version=version,
     author='Catotron',
     description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'protobuf',
