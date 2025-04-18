d0fd9089f96076b47dca9a70e58dc281ad6ea35f
CatotronExists <cam29706@gmail.com>
2025-04-18 15:27:50 +1000

    Update Basic Script example

--- examples/Basic Script/main.py
+++ examples/Basic Script/main.py
@@ -3,24 +3,22 @@
 import LiveApex
 import asyncio
 
-websocket_data = {'host': '127.0.0.1', 'port': 7777}
-
 async def filter_events(event):
     if event != None:
         if 'category' in event:
             if event['category'] == "init":
-                await LiveApex.Lobby.sendChatMessage("[LiveApex] Startup Successful", websocket_data)
-                await LiveApex.Lobby.getPlayers(websocket_data)
+                await LiveApex.Lobby.sendChatMessage("[LiveApex] Startup Successful")
+                await LiveApex.Lobby.getPlayers()
 
             # Your program should roughly follow this structure...
             # running a function after a certain event is received
 
 
-async def api_socket():
+async def main():
     # Start the API
-    api_task = asyncio.create_task(LiveApex.Core.startLiveAPI(websocket_data))
-    listener_task = asyncio.create_task(LiveApex.Core.startListener(filter_events, websocket_data))
+    api_task = asyncio.create_task(LiveApex.Core.startLiveAPI())
+    listener_task = asyncio.create_task(LiveApex.Core.startListener(filter_events))
 
     await asyncio.gather(api_task, listener_task)
 
-asyncio.run(api_socket())
+asyncio.run(main())
